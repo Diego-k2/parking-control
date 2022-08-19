1bf36198cd50876643cca1ee3ef4a41ef8fbcfca
Diego-k2 <degosantosiva@gmail.com>
2022-08-19 13:53:19 -0300

    primeiro commit

---
+++ src/test/java/br/com/pi/parkingcontrol/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pi.parkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
